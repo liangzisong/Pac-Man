9a32706390f47f29a55f049fdd2a973b8526ed58
liang <18462146261@163.com>
2018-09-05 16:42:35 +0800

    吃豆人可以正常移动

--- src/man/PacMan.java
+++ src/man/PacMan.java
@@ -2,21 +2,36 @@ package man;
 
 import java.awt.AWTException;
 import java.awt.Robot;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class PacMan {
 
 	public static void main(String[] args) {
-		StringBuffer background = new StringBuffer("* * * * * * * * * * * * * * *");
 		Scanner input = new Scanner(System.in);
 		//行
 		int manRow = 0;
 		//列
 		int manList = 14;
+		//移动方向
 		String move = "wu";
+        //初始化
+		String[][] str = new String[15][15];
+		for(int i=0;i<str.length;i++) {
+			for (int j=0;j<str.length;j++) {
+//				if(i/2==0){
+//					List<Integer> list = getDiffNO(15);
+////					int Random1 = new Random().nextInt(15);
+//					str[list.get(0)][list.get(1)]="@ ";
+//					//continue;
+//				}
+				
+				str[i][j]="* ";
+			}
+		}
         while (true) {
         	Robot r = null;
         	 try {
@@ -30,13 +45,7 @@ public class PacMan {
             //利用nextXXX()方法输出内容
             String in = input.nextLine();
             System.out.print("str="+in);
-            //初始化
-    		String[][] str = new String[15][15];
-    		for(int i=0;i<str.length;i++) {
-    			for (int j=0;j<str.length;j++) {
-    				str[i][j]="* ";
-    			}
-    		}
+
     		System.out.println("move="+move);
     			switch (in) {
 				case "d":
@@ -55,7 +64,7 @@ public class PacMan {
 				default:
 					break;
 				}
-    		boolean db = false,ab = false,sb = false,wb = false;
+    		boolean b = false;
     		if(manRow == 15) {
 				manRow = 13;
 			}
@@ -63,54 +72,73 @@ public class PacMan {
     			manList = 13;
 			}
     		str[manList][manRow]="c ";
-    		
+    		for(int i=0;i<str.length;i++) {
+    			for (int j=0;j<str.length;j++) {
+    				if (!"@ ".equals(str[i][j])) {
+    					if(i==str.length-1&&j==str.length-1) {
+        					System.out.println("if (\"@ \".equals(str[i][j])) {");
+        					List<Integer> list = getDiffNO(15);
+//        					int Random1 = new Random().nextInt(15);
+        					str[list.get(0)][list.get(1)]="@ ";
+        					break;
+    					}
+
+    				}
+    			}
+    		}
     		for(int i=0;i<str.length;i++) {
     			for (int j=0;j<str.length;j++) {
     				//1
-					if("d".equals(move) && db==false) {
+					if("d".equals(move) && b==false) {
 						if(manRow >= 14) {
 							manRow = 13;
 						}
 						manRow++;
 						System.out.println("manRow="+manRow);
 						str[manList][manRow]="c ";
+						//消影
+						str[manList][manRow-1]="* ";
 						move = "d";
-						db=true;
+						b=true;
 					}
 					//2
-					if("w".equals(move) && wb==false) {
+					if("w".equals(move) && b==false) {
 						if(manList <= 0) {
 							manList = 1;
 						}
 						manList--;
 						System.out.println("manRow="+manRow);
-						str[manList][manRow]="c ";
+						str[manList][manRow]="u ";
+						str[manList+1][manRow]="* ";
 						move = "w";
-						wb=true;
+						b=true;
 					}
 					//3
-					if("a".equals(move) && ab==false) {
+					if("a".equals(move) && b==false) {
 						if(manRow <= 0) {
 							manRow = 1;
 						}
 						manRow--;
 						System.out.println("manRow="+manRow);
-						str[manList][manRow]="c ";
+						str[manList][manRow]="D ";
+						str[manList][manRow+1]="* ";
 						move = "a";
-						ab=true;
+						b=true;
 					}
 					//4
-					if("s".equals(move) && sb==false) {
+					if("s".equals(move) && b==false) {
 						if(manList >= 14) {
 							manList = 13;
 						}
 						manList++;
 						System.out.println("manRow="+manRow);
-						str[manList][manRow]="c ";
+						str[manList][manRow]="n ";
+						str[manList-1][manRow]="* ";
 						move = "s";
-						sb=true;
+						b=true;
 					}
     			System.out.print(str[i][j]);
+    			//str[manList][manRow]="* ";
     			}
     			System.out.println();
     		}
@@ -126,14 +154,22 @@ public class PacMan {
 		//clear();
 	}
 	
-	public static void a() {
-		
-	}
-	
 	public static void clear(){
         for(int i=0;i<30;i++) {
         	System.out.println();
         }
 
+    }
+    public static List<Integer> getDiffNO(int n){
+    	Random r = new Random();
+    	List<Integer> list = new ArrayList<Integer>();
+        int i;
+        while(list.size() < 15){
+            i = r.nextInt(15);
+            if(!list.contains(i)){
+                list.add(i);
+            }
+        }
+		return list;
     }
 }
